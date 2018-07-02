1b921049578cd7401f4fb18330154c098a302b1d
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-07-02 22:52:25 +0530

    uh

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -22,7 +22,7 @@ class login extends CI_Controller
 		if ($this->form_validation->run() == FALSE)
         {
 			
-			redirect($_SERVER['HTTP_REFERER']);
+			redirect('login/index');
 		}
 		else
 		{
