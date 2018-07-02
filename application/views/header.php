362d6091697f5e51e6dde8c97e949d50a987e651
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-07-02 12:44:15 +0530

    ug

--- application/views/header.php
+++ application/views/header.php
@@ -42,103 +42,9 @@
 </head>
 
 <body>
-
-	<!--1st header
-		<div id="wrapper">
-
-			<div class="top-bar4">
-
-				<div class="container">
-
-					<div class="logo"><a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>media/images/logonw.png" width="130" height="70" alt="descipline"></a></div>
-
-					<div class="right-panel">
-						<ul class="login-list">
-							<?php if ($this->session->userdata('u_card')){ ?>
-							<li><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i><?php echo $result = substr($this->session->userdata('u_name'), 0, 6); ?></a></li>
-
-							<li><a href="<?php echo base_url();?>index.php/ras/logout" ><i class="fa fa-sign-in" aria-hidden="true"></i>logout</a></li>
-	           				 <?php } else{?>
-							<li><a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-sign-in" aria-hidden="true"></i>login</a></li>
-							<?php }?>
-						</ul>
-
-					</div>
-
-				</div>
-
-			</div>
-
-			
-
-			<header id="header2" class="header4 header-yellow">
-
-				<nav class="navbar navbar-default">
-
-					<div class="container-fluid">
-
-						<!-- Brand and toggle get grouped for better mobile display --
-
-						<div class="navbar-header">
-
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-
-								<span class="sr-only">Toggle navigation</span>
-
-								<span class="icon-bar"></span>
-
-								<span class="icon-bar"></span>
-
-								<span class="icon-bar"></span>
-
-							</button>
-
-						</div>
-
-
-
-						<!-- Collect the nav links, forms, and other content for toggling --
-
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-							<ul class="nav navbar-nav">
-
-								<li><a href="<?php echo base_url();?>">home</a>
-								</li>
-
-								<li><a href="<?php echo base_url();?>index.php/ras/about">About Us</a></li>
-
-								<li><a href="<?php echo base_url();?>index.php/ras/courses">courses</a></li>
-
-								<li><a href="<?php echo base_url();?>index.php/ras/results">Results</a></li>
-
-								<li class="active"><a href="<?php echo base_url();?>index.php/ras/student">student zone</a></li> 
-
-								<li class="last"><a href="<?php echo base_url();?>index.php/ras/contact">contact</a>
-
-								</li>
-
-							</ul>
-
-						</div><!-- /.navbar-collapse -->
-
-					</div><!-- /.container-fluid --
-
-				</nav>
-
-				
-
-				<div class="tel home layer"><i class="fa fa-phone" aria-hidden="true"></i><a href="#">(+91) 79031 35200</a></div>
-
-				
-
-			</header>
-	<--heade-->
-
-
-	<div id="wrapper">
-		<div class="top-bar2">
-			<div class="container">
+    <div id="wrapper ">
+		<div class="top-bar2 ">
+			<div class="container hidden-xs">
 				<ul class="social-icons">
 					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
 					<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
@@ -155,7 +61,7 @@
 			</div>
 		</div>
 		
-		<header id="header2">
+		<header id="header2" class="">
 			<div class="logo"><a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>media/images/logotext.png" width="184" height="20" alt=""></a></div>
 			
 			<nav class="navbar navbar-default">
